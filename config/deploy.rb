3abfdaa30b9adc19573dd143f123801c34313292
hab278 <hab278@nyu.edu>
2015-02-02 15:43:44 -0500

    Updated formaggio, use latest ruby

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,8 @@ require 'formaggio/capistrano'
 set :app_title, "rooms"
 set :recipient, "rooms.admin@library.nyu.edu"
 
+set :rvm_ruby_string, "ruby-2.1.5"
+
 namespace :reporting do
   desc "Startup delayed jobs script"
   task :cache do
